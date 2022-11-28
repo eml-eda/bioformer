834ea08bd3cce34a14f6b59524ad1e5275fcfd71
ABurrello <burry94>
2022-11-28 15:10:53 +0100

    fixed test accuracy

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from utils.configs import configs_pretrain, configs_finetune
 
 PROCESSES = 1
 save_model_every_n = 50
-name_prefix = f"vit_dim64_h8_"
+name_prefix = f"artifacts/vit_dim64_h8_"
 
 def extend_results(results, result):
     if results is None:
@@ -37,7 +37,7 @@ def main_pretraining(chunk_idx):
     assert len(add_sub) == 1
     ds_add_sub = DB6MultiSession(folder=os.path.expanduser(dataset_folder), subjects=add_sub, sessions=list(range(n_sessions)), minmax=(ds.X_min, ds.X_max), n_classes='7+1', steady=True, image_like_shape=True).to(device)
     test_ds = DB6MultiSession(folder=os.path.expanduser(dataset_folder), subjects=subjects, sessions=[n_sessions], minmax=(ds.X_min, ds.X_max), n_classes='7+1', steady=True, image_like_shape=True).to(device)
-    minmax_picklename = f'ds_minmax_sessions={n_sessions}subjects={ ",".join(map(str, subjects)) }.pickle'
+    minmax_picklename = f'artifacts/ds_minmax_sessions={n_sessions}subjects={ ",".join(map(str, subjects)) }.pickle'
     if not os.path.isfile(minmax_picklename):
         dump([ds.X_min, ds.X_max], open(minmax_picklename, 'wb'))
     losses_accs_ = []
@@ -87,7 +87,7 @@ def main_finetune(chunk_idx):
                             for i in test_sessions]
     results_ = []
     for i, config in enumerate(configs, start=1):
-        config['pretrained'] = f"../vit_dim64_h8_{subject - 1}_epoch100.pth"
+        config['pretrained'] = f"../artifacts/vit_dim64_h8_{subject - 1}_epoch100.pth"
         results = {}
         results['subject'] = subject
         results['steady'] = steady
@@ -97,7 +97,7 @@ def main_finetune(chunk_idx):
         results['test_sessions'] = test_sessions
         result = {}
         net = ViT(**config)
-        config['pretrained'] = f"vit_dim64_h8_{9 - (subject - 1)}_epoch100.pth"
+        config['pretrained'] = f"artifacts/vit_dim64_h8_{9 - (subject - 1)}_epoch100.pth"
         if config['pretrained'] is not None:
             net.load_state_dict(torch.load(config['pretrained']))
             print("Loaded checkpoint", config['pretrained'])
